4fc93205588d26941bcf49529aa00528b2f7ec4d
Kaique <kaique.lopes3271@gmail.com>
2021-09-13 14:57:59 -0300

    String & Integer

--- Swift_Playground.playground/Contents.swift
+++ Swift_Playground.playground/Contents.swift
@@ -5,9 +5,17 @@ var name = "Kaique Lopes"
 var lastName = "Oliveira"
 var age = 27
 var nameVar = "Kaique"
+
 // MARK: - Constantes
 
 let pi = 3.1415
 let ageLet = 27
-
 print("Meu nome é \(nameVar) e minha idade é \(ageLet) anos ")
+
+// MARK: - Strings
+
+var brand = "Nike"
+print(brand)
+
+// MARK: - Integer
+var number = 10
