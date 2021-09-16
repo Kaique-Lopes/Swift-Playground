63edfa783ebddefe62bedd3e999d3ee1fbe88ed8
Kaique <kaique.lopes3271@gmail.com>
2021-09-16 14:14:16 -0300

    Switchs

--- Swift_Playground.playground/Contents.swift
+++ Swift_Playground.playground/Contents.swift
@@ -358,6 +358,46 @@ default:
     print("Tempo Muito Quente")
 }
 
+var logado = false
+
+switch logado {
+case true:
+    print("Usuário Logado")
+case false:
+    print("Usuario Deslogado")
+}
+
+var maioridade = 18
+
+switch maioridade {
+case 0..<4:
+    print("Bebe")
+case 4..<7:
+    print("Bebe em Crescimento")
+case 7..<11:
+    print("Criança")
+case 11..<15:
+    print("Pré Adolescente")
+case 15..<18:
+    print("Adolescente")
+case 18..<100:
+    print("Adulto")
+default:
+    print("Mumia")
+}
+
+var piscinaOlimpica: UInt = 180
+switch piscinaOlimpica {
+case 0..<50:
+    print("Piscina Amadora")
+case 50..<100:
+    print("Piscina Rasa")
+case 100..<150:
+    print("Piscina Media-rasa")
+default:
+    print("Piscina Olimpica")
+}
+
 
 // MARK: - Functions
 func countryNames ( _ ida: String, _ chegada: String) -> String {
