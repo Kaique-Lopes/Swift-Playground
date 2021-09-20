1de0d620565d0e761270ccc9a3066339f1dd0f08
Kaique <kaique.lopes3271@gmail.com>
2021-09-20 15:29:37 -0300

    Class

--- Swift_Playground.playground/Pages/Class.xcplaygroundpage/Contents.swift
+++ Swift_Playground.playground/Pages/Class.xcplaygroundpage/Contents.swift
@@ -33,4 +33,32 @@ var colecaoDeMesas: [Mesa] = [mesaDaSala, mesaQuarto]
 for mesa in colecaoDeMesas {
     print(mesa.cor)
 }
-//: [Next](@next)
+
+
+// MARK: - Orientação a Objetos
+
+class User {
+    var name: String
+    var age: UInt
+    var photo: String
+    var yearActual: UInt
+    
+    init(name: String, age: UInt, photo: String, yearActual: UInt) {
+        self.name = name
+        self.age = age
+        self.photo = photo
+        self.yearActual = yearActual
+    }
+    
+    func login() {
+        print("Olá, meu nome é \(name) e eu nasci em \(yearActual - age)")
+    }
+}
+
+let userA = User(name: "Kaique", age: 27, photo: "Santos", yearActual: 2021)
+
+let userB = User(name: "Joao", age: 11, photo: "Bahia", yearActual: 2021)
+
+let userC = User(name: "Lucas", age: 15, photo: "Ceará", yearActual: 2021)
+ 
+userA.login()
