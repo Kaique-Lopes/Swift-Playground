7cf5648158f8fe8e04a9cf1e768efe11f2e6dd8b
Kaique <kaique.lopes3271@gmail.com>
2021-09-21 11:46:09 -0300

    Enum & rawValue

--- Swift_Playground.playground/Pages/Enums.xcplaygroundpage/Contents.swift
+++ Swift_Playground.playground/Pages/Enums.xcplaygroundpage/Contents.swift
@@ -2,9 +2,23 @@
 
 import Foundation
 
-enum RosaDosVentos {
-    case norte
-    case sul
-    case leste
-    case oeste
+enum RosaDosVentos : String {
+    case norte = "Norte"
+    case sul = "Sul"
+    case leste = "Leste"
+    case oeste = "Oeste"
+}
+
+var ventoLeste = RosaDosVentos.leste
+
+var ventoSul: RosaDosVentos = .sul
+
+var ventoNorte : RosaDosVentos = .norte
+
+var ventoOeste: RosaDosVentos = .oeste
+
+if ventoSul == .sul {
+    print("O vento está em direção ao \(ventoSul.rawValue) !")
+} else {
+    print("O vento não está indo para o Sul !")
 }
