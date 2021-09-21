ffe14e1e5d26f40d741368ef4c1b6ea2f5cdae68
Kaique <kaique.lopes3271@gmail.com>
2021-09-21 11:52:14 -0300

    Enum & Else If

--- Swift_Playground.playground/Pages/Enums.xcplaygroundpage/Contents.swift
+++ Swift_Playground.playground/Pages/Enums.xcplaygroundpage/Contents.swift
@@ -9,16 +9,26 @@ enum RosaDosVentos : String {
     case oeste = "Oeste"
 }
 
-var ventoLeste = RosaDosVentos.leste
 
-var ventoSul: RosaDosVentos = .sul
+var ventoSul: RosaDosVentos = .leste
+
+/*
 
 var ventoNorte : RosaDosVentos = .norte
-
+var ventoLeste = RosaDosVentos.leste
 var ventoOeste: RosaDosVentos = .oeste
+
+*/
 
 if ventoSul == .sul {
     print("O vento está em direção ao \(ventoSul.rawValue) !")
-} else {
-    print("O vento não está indo para o Sul !")
+    
+} else if ventoSul == .norte {
+    print("O vento não está indo para o \(ventoSul.rawValue)!")
+    
+} else if ventoSul == .leste {
+    print("O vento está em direção ao \(ventoSul.rawValue) !")
+    
+} else if ventoSul == .oeste {
+    print("O vento está em direção ao \(ventoSul.rawValue) !")
 }
