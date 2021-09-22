ca24c80f6f6f4ea6d957ac63a7d46b1a1b04474c
Kaique <kaique.lopes3271@gmail.com>
2021-09-22 10:34:03 -0300

    Dictionary

---
+++ Swift_Playground.playground/Pages/Dictionary.xcplaygroundpage/Contents.swift
@@ -0,0 +1,9 @@
+//: [Previous](@previous)
+
+import Foundation
+
+var dictionary = ["Chave1": 1]
+dictionary["Chave1"]
+
+var dictionary2 : [String: Int] = ["Number1": 1, "Number2": 2, "Number3": 3]
+dictionary2["Number3"]
