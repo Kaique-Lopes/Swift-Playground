38a537e4ae02db28571902cdc7a34fc060db0c6a
Kaique <kaique.lopes3271@gmail.com>
2021-09-22 10:36:59 -0300

    Dictionary Methods

--- Swift_Playground.playground/Pages/Dictionary.xcplaygroundpage/Contents.swift
+++ Swift_Playground.playground/Pages/Dictionary.xcplaygroundpage/Contents.swift
@@ -7,3 +7,12 @@ dictionary["Chave1"]
 
 var dictionary2 : [String: Int] = ["Number1": 1, "Number2": 2, "Number3": 3]
 dictionary2["Number3"]
+
+dictionary2["Number4"] = 4
+
+dictionary2["Number4"]
+
+dictionary2.updateValue(5, forKey: "Number5")
+print(dictionary2)
+
+
